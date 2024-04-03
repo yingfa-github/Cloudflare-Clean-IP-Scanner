6a0a348ddd2a42ac86d092a0c0ffc71cb41763c2
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-04-03 16:34:20 +0330

    update progress.go

--- utils/progress.go
+++ utils/progress.go
@@ -1,25 +1,24 @@
 package utils
 
 import (
-	"fmt"
-
-	"github.com/cheggaaa/pb/v3"
+        "fmt"
+        "github.com/cheggaaa/pb/v3"
 )
 
 type Bar struct {
-	pb *pb.ProgressBar
+        pb *pb.ProgressBar
 }
 
 func NewBar(count int, MyStrStart, MyStrEnd string) *Bar {
-	tmpl := fmt.Sprintf(`{{counters . }} {{ bar . "[" "-" (cycle . "↖" "↗" "↘" "↙" ) "_" "]"}} %s {{string . "MyStr" | green}} %s `, MyStrStart, MyStrEnd)
-	bar := pb.ProgressBarTemplate(tmpl).Start(count)
-	return &Bar{pb: bar}
+        tmpl := fmt.Sprintf(`{{counters . }} {{ bar . "[" "-" (cycle . "↖" "↗" "↘" "↙" ) "_" "]"}} %s {{string . "MyStr" | green}} %s `, MyStrStart, MyStrEnd)
+        bar := pb.ProgressBarTemplate(tmpl).Start(count)
+        return &Bar{pb: bar}
 }
 
 func (b *Bar) Grow(num int, MyStrVal string) {
-	b.pb.Set("MyStr", MyStrVal).Add(num)
+        b.pb.Set("MyStr", MyStrVal).Add(num)
 }
 
 func (b *Bar) Done() {
-	b.pb.Finish()
+        b.pb.Finish()
 }
